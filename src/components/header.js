09df62a091eba02f03e09af20d255f52cc95083e
omarderas <bz.inlandtours@gmail.com>
2023-06-13 23:20:13 -0600

    header updates

--- src/components/header.js
+++ src/components/header.js
@@ -53,7 +53,7 @@ const Header = ({ siteTitle }) => (
       
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-        National Assembly
+        Parliament
         <span className="desc">
           Useful Info
         </span>
@@ -62,18 +62,18 @@ const Header = ({ siteTitle }) => (
           <div className="row">
             <div className="col-sm-4">
             <a href="" class="sub-heading">House of Representatives</a>      
-              <a class="dropdown-item" href="">Present Members</a>
+              <a class="dropdown-item" href="">House Members</a>
               <a class="dropdown-item" href="">Past Speakers</a>
-              <a class="dropdown-item" href="">Standing Orders</a>
+              <a class="dropdown-item" href="">HOR Standing Orders</a>
               <a class="dropdown-item" href="">Orders of The Day</a>
               <a class="dropdown-item" href="">Draft Bills</a>
               </div>
             <div className="col-sm-4">
-            <a href="" class="sub-heading">The Senate</a>
+            <a href="" class="sub-heading">Senate</a>
              
-              <a class="dropdown-item" href="">Present Members</a>
+              <a class="dropdown-item" href="">Members</a>
               <a class="dropdown-item" href="">Past Presidents</a>
-              <a class="dropdown-item" href="">Standing Orders</a>
+              <a class="dropdown-item" href="">Senate Standing Orders</a>
               <a class="dropdown-item" href="">Orders of The Day</a>
           
               </div>
@@ -82,6 +82,8 @@ const Header = ({ siteTitle }) => (
             
               <a class="dropdown-item" href="">House Verbatim</a>
               <a class="dropdown-item" href="">Senate Verbatim</a>
+              <a class="dropdown-item" href="">House Orders</a>
+              <a class="dropdown-item" href="">Senate Orders</a>
               <a class="dropdown-item" href="">HR Meetings Minutes</a>
               <a class="dropdown-item" href="">Senate Meetings Minutes</a>
            
@@ -100,16 +102,17 @@ const Header = ({ siteTitle }) => (
 
           <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-        Committee
+        Media
         <span className="desc">
-          View Committee
+          Visit
         </span>
         </a>
         <div id="menu-main secondary" class="dropdown-menu" aria-labelledby="navbarDropdown">
           <div className="row">
             <div className="col-sm-4">
           
-              <a class="dropdown-item" href="">Standing Committee Members</a>
+              <a class="dropdown-item" href="">Live Streaming</a>
+              <a class="dropdown-item" href="">Gallery</a>
             
               </div>
         
@@ -133,24 +136,67 @@ const Header = ({ siteTitle }) => (
   <div className="row secondary">
     <ul className="secondary-menu">
       <li>
-        <a href="">
-          Acts of Parliament
-        </a>
-      </li>
-      <li>
-        <a href="">
-          Administration
-        </a>
-      </li>
-      <li>
-        <a href="">
-          Presiding Officers
-        </a>
-      </li>
-      <li>
-        <a href="">
-          History of Legislature
-        </a>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                National Assembly
+              </a>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <a class="dropdown-item" href="">How Laws are Made</a>
+              <a class="dropdown-item" href="">People in Parliament</a>
+              <a class="dropdown-item" href="">History of Legislature</a>
+              <a class="dropdown-item" href="">Presiding Officers</a>
+              <a class="dropdown-item" href="">The Mace</a>
+              <a class="dropdown-item" href="">Visit Parliament</a>
+             </div>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Committees
+              </a>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <a class="dropdown-item" href="">Standing Committees</a>
+              
+              </div>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Administration
+              </a>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <a class="dropdown-item" href="">Office of the CLerk</a>
+              <a class="dropdown-item" href="">Staff</a>
+              
+              </div>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Resources
+              </a>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <a class="dropdown-item" href="">Communique</a>
+              <a class="dropdown-item" href="">Acts</a>
+              <a class="dropdown-item" href="">Draft Bills</a>
+              <a class="dropdown-item" href="">Budgets</a>
+              <a class="dropdown-item" href="">Reports</a>
+              <a class="dropdown-item" href="">News</a>
+              
+              </div>
+            </li>
+          </ul>
+        
+        </div>
+
+      </nav>
       </li>
       <li id="social-block">
           <ul className="social">
